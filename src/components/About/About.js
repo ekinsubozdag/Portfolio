c280821c603a13402bb9141cbe3ce2b205fa72f5
ekinsubozdag <32195270+ekinsubozdag@users.noreply.github.com>
2024-09-17 12:05:54 +0300

    Update CV

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,56 +6,40 @@ import './About.css'
 const About = () => {
     const {name, role, description, resume, social} = about
 
-    return (
-        <div className='about center'>
-            {name && (
-                <h1>
-                    Hi, I am <span className='about__name'>{name}</span>
-                </h1>
-            )}
+    return (<div className='about center'>
+        {name && (<h1>
+            Hi, I am <span className='about__name'>{name}</span>
+        </h1>)}
 
-            {role && <h2 className='about__role'>A {role}</h2>}
-            <p className='about__desc'>{
-                description.map((line) =>
-                    <p> {line} </p>
-                )
-            }</p>
+        {role && <h2 className='about__role'>A {role}</h2>}
+        <p className='about__desc'>{description.map((line) => <p> {line} </p>)}</p>
 
-            <div className='about__contact center'>
-                {resume && (
-                    <a href='/Portfolio/EkinsuBozdagCV.pdf' target="_blank" download>
+        <div className='about__contact center'>
+            {resume && (<a href='/Portfolio/EkinsuBozdag_CV.pdf' target="_blank" download>
           <span type='button' className='btn btn--outline'>
               Resume
             </span>
-                    </a>
-                )}
+            </a>)}
 
-                {social && (
-                    <>
-                        {social.github && (
-                            <a
-                                href={social.github}
-                                aria-label='github'
-                                className='link link--icon'
-                            >
-                                <GitHubIcon/>
-                            </a>
-                        )}
+            {social && (<>
+                {social.github && (<a
+                    href={social.github}
+                    aria-label='github'
+                    className='link link--icon'
+                >
+                    <GitHubIcon/>
+                </a>)}
 
-                        {social.linkedin && (
-                            <a
-                                href={social.linkedin}
-                                aria-label='linkedin'
-                                className='link link--icon'
-                            >
-                                <LinkedInIcon/>
-                            </a>
-                        )}
-                    </>
-                )}
-            </div>
+                {social.linkedin && (<a
+                    href={social.linkedin}
+                    aria-label='linkedin'
+                    className='link link--icon'
+                >
+                    <LinkedInIcon/>
+                </a>)}
+            </>)}
         </div>
-    )
+    </div>)
 }
 
 export default About
